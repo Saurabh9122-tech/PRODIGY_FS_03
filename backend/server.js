e0ffe8d82c03562d188cd123a46af66ab0935cd6
Saurabh9122-tech <ss061959@gmail.com>
2025-06-28 12:39:44 +0530

    fix the seed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const productRoutes = require('./routes/productRoutes');
+const productRoutes = require('./routes/producttemp');
 
 const app = express();
 app.use(cors());
